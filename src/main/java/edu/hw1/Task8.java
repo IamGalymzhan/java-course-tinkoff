75d5b9b29fb34c205c923b7e38a2f84057bfa159
Galymzhan Zhangazy <zhangazy2004@gmail.com>
2023-10-16 02:54:59 +0600

    hw1-v2

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -5,22 +5,24 @@ public class Task8 {
 
     }
 
-    public static final int BOARD_SIZE = 8;
+    private static final int BOARD_SIZE = 8;
+
+    private static final int[] DX = {2, 2, 1, 1, -2, -2, -1, -1};
+    private static final int[] DY = {1, -1, 2, -2, 1, -1, 2, -2};
 
     private static boolean isValid(int i, int j) {
         return i >= 0 && i < BOARD_SIZE && j >= 0 && j < BOARD_SIZE;
     }
-    public static boolean knightBoardCapture (int[][] array) {
-        int dx[] = {2, 2, 1, 1, -2, -2, -1, -1};
-        int dy[] = {1, -1, 2, -2, 1, -1, 2, -2};
+
+    public static boolean knightBoardCapture(int[][] array) {
         for (int i = 0; i < BOARD_SIZE; i++) {
             for (int j = 0; j < BOARD_SIZE; j++) {
                 if (array[i][j] == 0) {
                     continue;
                 }
                 for (int k = 0; k < BOARD_SIZE; k++) {
-                    int x = i + dx[k];
-                    int y = j + dy[k];
+                    int x = i + DX[k];
+                    int y = j + DY[k];
                     if (isValid(x, y)) {
                         if (array[x][y] == 1) {
                             return false;
